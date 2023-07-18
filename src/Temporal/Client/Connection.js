c3f053cb4c9c047307ddbd8c8b59bb82a2f1d124
klarkc <walkerleite490@gmail.com>
2023-07-17 23:40:59 -0300

    style: LSP format

--- src/Temporal/Client/Connection.js
+++ src/Temporal/Client/Connection.js
@@ -3,15 +3,15 @@ import { Connection } from "@temporalio/client"
 export const connectionCtor = Connection
 
 export function connectImpl(ctor) {
-	return function (options) {
-		return function (onError, onSuccess) {
-				ctor.connect(ctor, options)
-					.then(onSuccess)
-					.catch(onError)
-				return function(cancelError, onCancelerError, onCancellerSuccess) {
-					// TODO cancel pending connection
-					onCancellerSuccess()
-				}
+	return function(options) {
+		return function(onError, onSuccess) {
+			ctor.connect(ctor, options)
+				.then(onSuccess)
+				.catch(onError)
+			return function(cancelError, onCancelerError, onCancellerSuccess) {
+				// TODO cancel pending connection
+				onCancellerSuccess()
+			}
 		}
 	}
 }
