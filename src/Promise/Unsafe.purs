6d5121984542e4479188d7120425ebdc95c23336
klarkc <walkerleite490@gmail.com>
2023-07-31 15:57:39 -0300

    feat: add compile warning on unsafe

--- src/Promise/Unsafe.purs
+++ src/Promise/Unsafe.purs
@@ -1,10 +1,15 @@
-module Promise.Unsafe (unsafeFromAff) where
+module Promise.Unsafe (class UnsafeWarning, unsafeFromAff) where
 
 import Prelude ((<<<))
 import Effect.Unsafe (unsafePerformEffect)
 import Effect.Aff (Aff)
 import Promise (class Flatten)
 import Promise.Aff (Promise, fromAff)
+import Prim.TypeError (class Warn, Text)
 
-unsafeFromAff :: forall a b. Flatten a b => Aff a -> Promise b
+class UnsafeWarning
+
+instance warn :: Warn (Text "Unsafe perform effect") => UnsafeWarning
+
+unsafeFromAff :: forall a b. UnsafeWarning => Flatten a b => Aff a -> Promise b
 unsafeFromAff = unsafePerformEffect <<< fromAff
